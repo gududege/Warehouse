def433feff4867ec477dab51062c109953ab387e
Jay <a18363006063@outlook.com>
2024-02-25 22:37:02 +0800

    Add remote git repo

--- docker-images/ansible-playbook-cron/docker-entrypoint.sh
+++ docker-images/ansible-playbook-cron/docker-entrypoint.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 # Update timeozne info
+set
 log() {
-	if ! $ENV_QUITE_MODE; then
+    if ! $ENV_QUITE_MODE; then
         echo -e "$1"
-	fi
+    fi
 }
-ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime \
-    && echo ${TZ} > /etc/timezone
+logerr() {
+    echo -e "$1"
+    exit $2
+}
+ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime &&
+    echo ${TZ} >/etc/timezone
 if [ $# -eq 0 ]; then
     # Exit if no any args
-    exit 1
+    logerr "No input arguments" 1
 else
     # Install need packages
     if [ -n "${ENV_APT_PACKAGES}" ]; then
         for pack in ${ENV_APT_PACKAGES}; do
-            if ! apt list --installed | grep $pack >> /dev/null; then
-                log "######### Installing Debian packages: $pack #########"  
+            if ! apt list --installed | grep $pack >>/dev/null; then
+                log "######### Installing Debian packages: $pack #########"
                 apt install -y $pack
             fi
         done
@@ -23,8 +28,8 @@ else
     # Install need packages
     if [ -n "${ENV_PIP_PACKAGES}" ]; then
         for pack in ${ENV_PIP_PACKAGES}; do
-            if ! pip list | grep $pack >> /dev/null; then
-                log "######### Installing pip package: $pack #########"        
+            if ! pip list | grep $pack >>/dev/null; then
+                log "######### Installing pip package: $pack #########"
                 pip --no-cache-dir install $pack
             fi
         done
@@ -32,28 +37,71 @@ else
     # Install need packages
     if [ -n "${ENV_ANSIBLE_COLLECTIONS}" ]; then
         for collection in ${ENV_ANSIBLE_COLLECTIONS}; do
-            if ! ansible-galaxy collection list | grep $collection >> /dev/null; then
+            if ! ansible-galaxy collection list | grep $collection >>/dev/null; then
                 log "######### Installing ansible collection: $collection #########"
                 ansible-galaxy collection install $collection
             fi
         done
     fi
-    # Print program info
+    # Change to workdir
+    cd $ENV_WORKDIR
+    # Get content from remote repository
+    if [ -n "${ENV_GIT_REPOSITORY}" ]; then
+        log "######### Get content from remote repository #########"
+        if ! $ENV_QUITE_MODE; then
+            if [ -n "${ENV_GIT_TOKEN}" ]; then
+{
+/usr/bin/expect <<EOF
+set timeout 120
+spawn git clone "${ENV_GIT_REPOSITORY}" "${ENV_WORKDIR}"
+expect "Username for"
+send "user\n"
+expect "Password for"
+send "${ENV_GIT_TOKEN}\n"
+expect eof
+catch wait result
+exit [lindex $result 3]
+EOF
+}
+            else
+                git clone ${ENV_GIT_REPOSITORY} ${ENV_WORKDIR}
+            fi
+        else
+            if [ -n "${ENV_GIT_TOKEN}" ]; then
+{
+expect <<EOF 
+set timeout 120
+spawn git clone "${ENV_GIT_REPOSITORY}" "${ENV_WORKDIR}"
+expect "Username for"
+send "user\n"
+expect "Password for"
+send "${ENV_GIT_TOKEN}\n"
+expect eof
+catch wait result
+exit [lindex $result 3]
+EOF
+}>/dev/null
+            else
+                git clone ${ENV_GIT_REPOSITORY} ${ENV_WORKDIR} >/dev/null
+            fi
+        fi
+        [ $? -ne 0 ] && logerr "Cannot clone remote repository" 2
+    fi
     if ! $ENV_QUITE_MODE; then
         log "######### Print ansible-playbook information #########"
-        ansible-playbook --version >> /proc/1/fd/1
-	fi 
+        { ansible-playbook --version; }
+    fi
     if [ -n "${ENV_CRON_EXPRESSIONS}" ]; then
-        # add an 
+        # add an
         log "######### Add below ansible-playbook task to cron  #########"
         log "${ENV_CRON_EXPRESSIONS} root ansible-playbook $@"
-        echo "${ENV_CRON_EXPRESSIONS} root ansible-playbook $@ >> /proc/1/fd/1" >> /etc/crontab
-        service cron restart >> /dev/null
+        echo "${ENV_CRON_EXPRESSIONS} root ansible-playbook $@ >> /proc/1/fd/1" >>/etc/crontab
+        service cron restart >>/dev/null
         log "######### Executing  #########"
-        tail -f >> /dev/null
+        tail -f >>/dev/null
     else
         # Excute command directly
         log "######### Execute ansible-playbook directly #########"
-        ansible-playbook $@
+        { ansible-playbook $@; }
     fi
 fi
